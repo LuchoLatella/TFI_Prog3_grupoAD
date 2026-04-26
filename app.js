699c492e5142d5edfe2bbc4bada6e44de4c5384a
LuchoLatella <lucho_latella@hotmail.com>
2026-04-26 17:06:12 -0300

    iniciado el README

---
+++ app.js
@@ -0,0 +1,12 @@
+// Creando la API Conexión a la base de datos
+
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+
+
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
