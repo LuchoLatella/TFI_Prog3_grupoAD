1ae24e82ad4f4380011886670254da4a9df16027
LuchoLatella <lucho_latella@hotmail.com>
2026-04-26 17:15:42 -0300

    iniciado app.js con conexión a API Puerto 3000

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
-// Creando la API Conexión a la base de datos
+// Creando la Conexión a la base de datos API
 
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
 
 
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-require('dotenv').config();
+app.get("/", (req, res) => {
+  res.send("API funcionando correctamente");          //en caso de que la API esté funcionando correctamente, muestr este mensaje 
+});
+
+const PORT = process.env.PORT || 3000;              //definimos el puerto en el que se ejecutará la API, si no se especifica, se usará el puerto 3000
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
+});
