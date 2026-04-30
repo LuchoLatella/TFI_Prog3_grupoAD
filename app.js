f85c9f225d3fc87fb03a0c15daad09dad4c04f28
LuchoLatella <lucho_latella@hotmail.com>
2026-04-30 17:31:17 -0300

    agregado el archivo .env

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 
 app.get("/", (req, res) => {
-  res.send("API funcionando correctamente");          //en caso de que la API esté funcionando correctamente, muestr este mensaje 
+  res.send("API funcionando correctamente");          //en caso de que la API esté funcionando correctamente, muestra este mensaje 
 });
 
 const PORT = process.env.PORT || 3000;              //definimos el puerto en el que se ejecutará la API, si no se especifica, se usará el puerto 3000
